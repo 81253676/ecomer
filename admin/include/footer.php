84128f17de1d8b5d1171129230752113a4f21228
DataJR <juandaniel280295@gmail.com>
2021-08-30 12:50:35 -0500

    se agrega el proyecto

---
+++ admin/include/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">&copy; 2020 Tienda Virtual - <a href="https://www.youtube.com/channel/UC0wE2sYJreqbSOYsH7q0x8Q"> DataJR</a> </b> Todos los derechos reservados.
+		</div>
+	</div>
---
+++ includes/brands-slider.php
@@ -0,0 +1,62 @@
+<div id="brands-carousel" class="logo-slider wow fadeInUp">
+		<h3 class="section-title">Marcas Reconocidas</h3>
+		<div class="logo-slider-inner">	
+			<div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/crest.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/Bosch-Logo.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/surtek.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/cemix.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/truper.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/pertul.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/dewalt.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/blak.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/foset.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+				
+				
+		    </div><!-- /.owl-carousel #logo-slider -->
+	</div><!-- /.logo-slider-inner -->
+	
+</div>
+<!-- /.logo-slider -->
